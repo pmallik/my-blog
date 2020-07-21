92c1b09b56700b517549bcbdf3f8fb85cf47901b
pmallik <puja_mallik@yahoo.com>
2020-07-20 21:57:17 -0500

    First commit

---
+++ src/build/precache-manifest.aa00e457191248ed9b4794da694f5c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204d465c26cab99a1c97d9920de333c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21413148981e93697ad6",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "300a592832da907970ef",
+    "url": "/static/js/2.3da9bc43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3da9bc43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21413148981e93697ad6",
+    "url": "/static/js/main.04a3e2e1.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
